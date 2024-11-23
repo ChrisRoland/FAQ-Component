b10985d81ad130a439285ac4376ed9c6bfa1f7a4
ChrisRoland <chrisebuberoland@gmail.com>
2024-11-23 19:59:50 +0100

    feat: complete

---
+++ script.js
@@ -0,0 +1,13 @@
+//Jquery
+$("button").on("click", function () {
+    $(this).closest(".faq").toggleClass("active");
+});
+
+//Vanilla JS
+// const toggle = document.querySelectorAll(".faq-toggle");
+
+// toggle.forEach((btn) => {
+//   btn.addEventListener("click", () => {
+//     btn.parentNode.classList.toggle("active");
+//   });
+// });
